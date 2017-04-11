4a2f458e488fd82db9d8e05b77722a864618b2a4
woodyan <aprilsnowyou@gmail.com>
2017-04-11 23:07:37 +0800

    init website

---
+++ js/main.js
@@ -0,0 +1,160 @@
+'use strict';
+
+var downoadLink = "http://a.app.qq.com/o/simple.jsp?pkgname=com.pix72";
+
+/* ---- type.js config ---- */
+$('.main-subtitle').typeIt({
+     strings: ["全球新鲜干货"],
+     speed: 200
+});
+
+/* ---- particles.js config ---- */
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 50,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+/* ---- popover config ---- */
+$("[data-toggle=popover]").popover({
+        trigger: 'hover',
+        placement: 'bottom',
+        html : true,
+        content: function () {
+            if($(window).innerWidth() > 768) {
+                return '<div class="qr"><img src="img/iTunes_QR.png" alt="world"></div>';
+            } else {
+                this.href = downoadLink;
+            }
+        }
+    }
+);
+
+/* ---- container animation---- */
+function showUp() {
+
+    var a = $(".container-logo").offset().top - $(document).scrollTop() - $(window).height();
+    var b = $(".container-logo").height() + $(window).height();
+
+    if(a > 0){
+        $(".container-logo").removeClass("showUp");
+    } else {
+        $(".container-logo").addClass("showUp");
+    }
+}
+
+$(document).scroll(showUp);
+
+/* ---- google-analytics ---- */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-XXXXX-Y', 'auto');
+ga('send', 'pageview');
